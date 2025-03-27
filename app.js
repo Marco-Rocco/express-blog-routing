f291de6c77ae0074d8e2ff23988682e84decdf15
Marco-Rocco <marcorocco28@icloud.com>
2025-03-27 13:08:29 +0100

    add: start server and create collection on postman

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 1100;
 
+app.get('/', (req, res) => {
+    console.log('response to client was sent for /');
+
+    res.send('server del blog-router')
+})
+
 app.listen(port, () => {
     console.log('server attivo su port ' + port);
-})
+});
+
