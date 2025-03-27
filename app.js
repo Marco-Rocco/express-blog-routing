46b9dce76e4c9ca5af83d51ba4d35faa116c4d10
Marco-Rocco <marcorocco28@icloud.com>
2025-03-27 13:14:37 +0100

    add: resposne for /posts

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 1100;
+//includi posts
+const posts = require('./posts.js')
 
 app.get('/', (req, res) => {
     console.log('response to client was sent for /');
 
     res.send('server del blog-router')
+})
+
+app.get('/posts', (req, res) => {
+    console.log('response to client was sent for /posts');
+
+    res.json(posts)
 })
 
 app.listen(port, () => {
